b254f1f1165dacc9cbd9ab080591b396b1a01354
Celina Jackson <celinanjackson@gmail.com>
2020-07-10 23:01:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,12 @@ class Song
     @@all
   end
 
+  def self.create(name)
+    song = self.new
+    song.name = name
+    song.artist = artist_name
+    @@all << self
+  end 
   def save
     self.class.all << self
   end
