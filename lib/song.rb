6447a9a0175bb12a686f95d1da8a08d79110861b
Celina Jackson <celinanjackson@gmail.com>
2020-07-10 23:16:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,7 +51,7 @@ class Song
     song = self.new
     song.name = (name.split(" - ")[1].chomp(".mp3"))
     song.artist_name = (name.split(" - ")[0])
-    @@all << song 
-    song 
+    @@all << song
+    song
   end 
 end
